2216339bc6a939c3a78896d883575e139713c630
Cristian-Sosa <64815792+Cristian-Sosa@users.noreply.github.com>
2022-05-20 10:21:57 -0300

    changes

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 const totalCalories = document.querySelector('.result h3');
-
 const goals = document.querySelectorAll('.goals li');
 
-// const
-
+var unit, gender, age, height, currentWeight, activity, goalWeight, calories
 
 document.addEventListener('click', (e) => {
     
@@ -12,19 +10,64 @@ document.addEventListener('click', (e) => {
 
         console.log('Mostrando resultados...');
 
-        showResults()
+        takeValues();
+        calculateCalories()
+        showResults();
+
+
     }
 });
 
 
 const showResults = () => {
-    totalCalories.textContent = 4000
+    totalCalories.textContent = 4000;
 
     goals.forEach(goal => {
         if (goal.className.match('weight_goal-modified')) {
-            goal.textContent = `150 kg`
+            goal.textContent = goalWeight;
         } else {
-            goal.textContent = 'On 3 months'
+            goal.textContent = 'On 3 months';
         }
      });
 };
+
+
+const takeValues = () => {
+    // itera sobre cada input name='units'
+    // Devuelve el checked en la variable unit
+    for (let i = 0; i < document.getElementsByName('units').length; i++) {
+        if (document.getElementsByName('units')[i].checked) {    
+            unit = document.getElementsByName('units')[i].id;
+            
+            // console.log(unit);
+
+            break;
+        };
+
+    };
+
+    // itera sobre cada input name='gender'
+    // Devuelve el checked en la variable gender
+    for (let i = 0; i < document.getElementsByName('gender').length; i++) {
+        if (document.getElementsByName('gender')[i].checked) {
+            gender = document.getElementsByName('gender')[i].id;
+            
+            // console.log(gender);
+
+            break;
+        };
+    };
+    
+    age = document.querySelector('.age input#age').value;
+    height = document.querySelector('.height input#height').value;
+    currentWeight = document.querySelector('.current_weight input#currect_weight').value;
+    activity = document.querySelector('.activity select#activity').value;
+    goalWeight = document.querySelector('.weight_goal input#weight_goal').value;
+
+};
+
+
+const calculateCalories = () => {
+    switch
+};
+
